99f5f7b66ebf23478ae3ca62b9a5dfc26d89ce8f
Robert Marianski <rob@marianski.com>
2026-02-03 09:55:44 -0500

    Add ability to quote live grep args

--- init.lua
+++ init.lua
@@ -190,6 +190,7 @@ require("nvim-treesitter.configs").setup({
 })
 
 local telescope_actions = require("telescope.actions")
+local lga_actions = require("telescope-live-grep-args.actions")
 require("telescope").setup({
     defaults = {
         -- Disable preview by default for faster performance
@@ -241,6 +242,11 @@ require("telescope").setup({
     extensions = {
         live_grep_args = {
             auto_quoting = true, -- Enable automatic quoting of search terms
+            mappings = {
+                i = {
+                    ["<C-k>"] = lga_actions.quote_prompt(),
+                },
+            },
         },
     },
 })
